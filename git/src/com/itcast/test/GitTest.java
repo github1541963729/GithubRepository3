16d7afb2ed9b2f20c552eb356e768d005b377daa
zhangsan <zhangsan@itcast.cn>
2022-05-27 14:27:32 +0800

    把本地工程添加到git本地仓库

---
+++ git/src/com/itcast/test/GitTest.java
@@ -0,0 +1,14 @@
+package com.itcast.test;
+
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("Git");
+        System.out.println("Git");
+        System.out.println("Git");
+        System.out.println("Git");
+        System.out.println("Git");
+        System.out.println("Git");
+        System.out.println("Git");
+    }
+}
